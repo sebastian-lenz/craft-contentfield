da8c6c7ebd94e05f0def2351ca2d2d7cb8f6a84f
Sebastian Lenz <github@sebastian-lenz.de>
2019-06-11 14:35:22 +0200

    Remove deprecated install sequence

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -18,26 +18,6 @@ class Install extends Migration
     ContentRecord::createTable($this, [
       'model' => $this->longText(),
     ]);
-
-    if (!$this->db->tableExists(ContentRecord::TABLE)) {
-      $this->createTable(ContentRecord::TABLE, [
-        'id'          => $this->primaryKey(),
-        'elementId'   => $this->integer()->notNull(),
-        'siteId'      => $this->integer()->notNull(),
-        'fieldId'     => $this->integer()->notNull(),
-        'dateCreated' => $this->dateTime()->notNull(),
-        'dateUpdated' => $this->dateTime()->notNull(),
-        'uid'         => $this->uid(),
-        'content'     => $this->longText(),
-      ]);
-
-      $this->createIndex(null, ContentRecord::TABLE, ['elementId', 'siteId'], true);
-      $this->createIndex(null, ContentRecord::TABLE, ['siteId'], false);
-      $this->createIndex(null, ContentRecord::TABLE, ['fieldId'], false);
-
-      $this->addForeignKey(null, ContentRecord::TABLE, ['elementId'], Table::ELEMENTS, ['id'], 'CASCADE', null);
-      $this->addForeignKey(null, ContentRecord::TABLE, ['fieldId'], Table::FIELDS, ['id'], 'CASCADE', null);
-    }
   }
 
   /**
