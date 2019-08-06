9fadbec9a21f79353b7a14120148d8c292f5ea28
Sebastian Lenz <github@sebastian-lenz.de>
2019-08-06 16:30:00 +0200

    Add `monospace`, `rows` and `placeholder` attributes to textarea

--- src/models/fields/TextAreaField.php
+++ src/models/fields/TextAreaField.php
@@ -2,6 +2,9 @@
 
 namespace lenz\contentfield\models\fields;
 
+use craft\base\ElementInterface;
+use lenz\contentfield\Plugin;
+
 /**
  * Class TextAreaField
  *
@@ -10,7 +13,72 @@ namespace lenz\contentfield\models\fields;
 class TextAreaField extends AbstractStringField
 {
   /**
+   * Whether to use a monospace font in the control panel
+   * or not.
+   *
+   * @var bool
+   */
+  public $monospace = false;
+
+  /**
+   * Placeholder text for the textarea element. Translated using the default
+   * translation method.
+   *
+   * @var string
+   */
+  public $placeholder = '';
+
+  /**
+   * The number of initially visible text lines.
+   *
+   * @var int
+   */
+  public $rows = 5;
+
+  /**
    * The internal name of this widget.
    */
   const NAME = 'textarea';
+
+
+  /**
+   * @inheritDoc
+   */
+  public function getEditorData(ElementInterface $element = null) {
+    return parent::getEditorData($element) + [
+      'monospace'   => $this->monospace,
+      'placeholder' => $this->getPlaceholder(),
+      'rows'        => $this->rows,
+    ];
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function rules() {
+    return array_merge(
+      parent::rules(),
+      [
+        [['monospace', 'rows'], 'required'],
+        ['monospace', 'boolean'],
+        ['placeholder', 'string'],
+        ['rows', 'integer'],
+      ]
+    );
+  }
+
+
+  // Private methods
+  // ---------------
+
+  /**
+   * @return string
+   */
+  private function getPlaceholder() {
+    $placeholder = trim((string)$this->placeholder);
+
+    return empty($placeholder)
+      ? ''
+      : Plugin::t($placeholder);
+  }
 }
