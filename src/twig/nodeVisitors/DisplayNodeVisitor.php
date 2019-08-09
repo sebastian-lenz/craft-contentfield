bdd2016f49f4d80c5a416b3dcb7b191e7b56a064
Sebastian Lenz <github@sebastian-lenz.de>
2019-08-09 14:44:18 +0200

    Fix an issue when inlining nested templates

--- src/twig/nodeVisitors/DisplayNodeVisitor.php
+++ src/twig/nodeVisitors/DisplayNodeVisitor.php
@@ -3,16 +3,10 @@
 namespace lenz\contentfield\twig\nodeVisitors;
 
 use Exception;
-use lenz\contentfield\models\schemas\TemplateSchema;
 use lenz\contentfield\twig\nodes\DisplayNode;
-use lenz\contentfield\twig\nodes\InlineIndexNode;
-use lenz\contentfield\twig\nodes\InlineTemplateNode;
 use lenz\contentfield\twig\nodes\SiblingsNode;
 use Throwable;
 use Twig\Environment;
-use Twig\Error\Error;
-use Twig\Error\LoaderError;
-use Twig\Error\SyntaxError;
 use Twig\Node\ModuleNode;
 use Twig\Node\Node;
 use Twig\NodeVisitor\AbstractNodeVisitor;
@@ -23,24 +17,9 @@ use Twig\NodeVisitor\AbstractNodeVisitor;
 class DisplayNodeVisitor extends AbstractNodeVisitor
 {
   /**
-   * @var InlineIndexNode
+   * @var DisplayNodeVisitorContext|null
    */
-  private $_inlineIndex;
-
-  /**
-   * @var ModuleNode[]
-   */
-  private $_moduleStack = [];
-
-  /**
-   * @var bool
-   */
-  private $_requiresIndex = false;
-
-  /**
-   * @var ModuleNode[]
-   */
-  static private $_templates = [];
+  private $_context = null;
 
 
   /**
@@ -49,15 +28,11 @@ class DisplayNodeVisitor extends AbstractNodeVisitor
    */
   protected function doEnterNode(Node $node, Environment $env) {
     if ($node instanceof ModuleNode) {
-      $this->_moduleStack[] = $node;
-      if (count($this->_moduleStack) == 1) {
-        $this->_inlineIndex = new InlineIndexNode();
-        $this->_requiresIndex = false;
-      }
+      $this->_context = new DisplayNodeVisitorContext($node, $this->_context);
     } elseif ($node instanceof DisplayNode) {
-      $this->attach($node, $env);
+      $this->attachDisplay($node, $env);
     } else if ($node instanceof SiblingsNode) {
-      $this->_requiresIndex = true;
+      $this->attachSibling();
     }
 
     return $node;
@@ -65,13 +40,18 @@ class DisplayNodeVisitor extends AbstractNodeVisitor
 
   /**
    * @inheritDoc
+   * @throws Exception
    */
   protected function doLeaveNode(Node $node, Environment $env) {
     if ($node instanceof ModuleNode) {
-      array_pop($this->_moduleStack);
-      if (count($this->_moduleStack) == 0 && $this->_requiresIndex) {
-        $node->getNode('class_end')->setNode('inline_index', $this->_inlineIndex);
+      if (
+        is_null($this->_context) ||
+        $this->_context->getModule() !== $node
+      ) {
+        throw new Exception('Invalid twig module hierarchy.');
       }
+
+      $this->_context = $this->_context->getParent();
     }
 
     return $node;
@@ -93,88 +73,33 @@ class DisplayNodeVisitor extends AbstractNodeVisitor
    * @param Environment $env
    * @throws Throwable
    */
-  protected function attach(DisplayNode $node, Environment $env) {
-    if ($node->usesIndexDisplay()) {
-      $this->_requiresIndex = true;
+  protected function attachDisplay(DisplayNode $node, Environment $env) {
+    $context = $this->_context;
+    if (is_null($context)) {
+      throw new Exception('Cannot compile display tag, context is missing.');
     }
 
     foreach ($node->getInlineSchemaCandidates() as $candidate) {
-      $callback = $this->tryInline($candidate, $env);
-
+      $callback = $context->tryInline($candidate, $env);
       if (!is_null($callback)) {
         $node->setInlinedSchema($candidate->qualifier, $callback);
-        $this->_inlineIndex->setInlinedSchema($candidate->qualifier, $callback);
       }
+    }
+
+    if ($node->usesIndexDisplay()) {
+      $context->requireIndex();
     }
   }
 
   /**
-   * @param TemplateSchema $schema
-   * @param Environment $env
-   * @return ModuleNode
-   * @throws Error
-   * @throws SyntaxError
-   * @throws LoaderError
+   * @throws Exception
    */
-  protected function compile(TemplateSchema $schema, Environment $env) {
-    $template = $schema->template;
-    if (!isset(self::$_templates[$template])) {
-      $source = $env->getLoader()->getSourceContext($schema->template);
-
-      try {
-        self::$_templates[$template] = $env->parse($env->tokenize($source));
-      } catch (Error $error) {
-        $error->setSourceContext($source);
-        throw $error;
-      } catch (Exception $error) {
-        throw new SyntaxError(
-          sprintf(
-            'An exception has been thrown during the compilation of a template ("%s").',
-            $error->getMessage()
-          ),
-          -1,
-          $source,
-          $error
-        );
-      }
+  protected function attachSibling() {
+    $context = $this->_context;
+    if (is_null($context)) {
+      throw new Exception('Cannot compile sibling tag, context is missing.');
     }
 
-    return self::$_templates[$template];
-  }
-
-  /**
-   * @param TemplateSchema $schema
-   * @param Environment $env
-   * @return string|null
-   * @throws Throwable
-   */
-  protected function tryInline(TemplateSchema $schema, Environment $env) {
-    $root = reset($this->_moduleStack);
-    if (!($root instanceof ModuleNode)) {
-      return null;
-    }
-
-    // Check whether we already have that template inlined
-    $classEnd = $root->getNode('class_end');
-    if ($classEnd->hasNode($schema->qualifier)) {
-      return $classEnd
-        ->getNode($schema->qualifier)
-        ->getAttribute('name');
-    }
-
-    // Compile the template
-    $subModule = $this->compile($schema, $env);
-
-    // Copy over all macros
-    $rootMacros = $root->getNode('macros');
-    foreach ($subModule->getNode('macros') as $name => $macroNode) {
-      $rootMacros->setNode($name, $macroNode);
-    }
-
-    // Create an inline template node
-    $node = new InlineTemplateNode($schema, $subModule);
-    $classEnd->setNode($schema->qualifier, $node);
-
-    return $node->getAttribute('name');
+    $context->requireIndex();
   }
 }
