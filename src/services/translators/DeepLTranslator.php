b53fc92fd14e4bb66f4b45436f376e630f32b84f
Sebastian Lenz <github@sebastian-lenz.de>
2025-08-26 12:19:11 +0200

    Add a language mapping helper to the DeepL translator

--- src/services/translators/DeepLTranslator.php
+++ src/services/translators/DeepLTranslator.php
@@ -14,12 +14,19 @@ class DeepLTranslator extends AbstractTranslator
   /**
    * @var string
    */
-  private string $_endpoint = self::DEFAULT_ENDPOINT;
+  private string $_authKey;
 
   /**
    * @var string
    */
-  private string $_authKey;
+  private string $_endpoint = self::DEFAULT_ENDPOINT;
+
+  /**
+   * @var string[]
+   */
+  private array $_localeReplacements = [
+    'en' => 'en-GB'
+  ];
 
   /**
    * The url of the API endpoint.
@@ -30,23 +37,22 @@ class DeepLTranslator extends AbstractTranslator
   /**
    * @return string
    */
-  public function getEndpoint(): string {
-    return empty($this->_endpoint) ? self::DEFAULT_ENDPOINT : $this->_endpoint;
-  }
-
-  /**
-   * @return string
-   */
   public function getAuthKey(): string {
     return $this->_authKey;
   }
 
   /**
-   * @param string $value
-   * @noinspection PhpUnused
+   * @return string
    */
-  public function setEndpoint(string $value): void {
-    $this->_endpoint = $value;
+  public function getEndpoint(): string {
+    return empty($this->_endpoint) ? self::DEFAULT_ENDPOINT : $this->_endpoint;
+  }
+
+  /**
+   * @return string[]
+   */
+  public function getLocaleReplacements(): array {
+    return $this->_localeReplacements;
   }
 
   /**
@@ -58,15 +64,60 @@ class DeepLTranslator extends AbstractTranslator
   }
 
   /**
+   * @param string $value
+   * @noinspection PhpUnused
+   */
+  public function setEndpoint(string $value): void {
+    $this->_endpoint = $value;
+  }
+
+  /**
+   * @param string|array $value
+   * @return void
+   */
+  public function setLocaleReplacements(string|array $value): void {
+    if (!is_array($value)) {
+      $replacements = [];
+      foreach (explode("\n", $value) as $line) {
+        $arguments = array_values(array_filter(array_map('trim', explode(':', $line, 2))));
+        if (count($arguments) == 2) {
+          $replacements[$arguments[0]] = $arguments[1];
+        }
+      }
+
+      $value = $replacements;
+    }
+
+    $this->_localeReplacements = $value;
+  }
+
+  /**
    * @inheritDoc
    */
   public function translate(string $sourceLanguage, string $targetLanguage, string $message): ?string {
     $translator = new Translator(App::parseEnv($this->getAuthKey()));
-    $result = $translator->translateText($message, $sourceLanguage, $targetLanguage, [
-      'tag_handling' => 'html'
-    ]);
+    $result = $translator->translateText(
+      texts: $message,
+      sourceLang: $sourceLanguage,
+      targetLang: $this->fixTargetLanguage($targetLanguage),
+      options: ['tag_handling' => 'html']
+    );
 
     return $result->text;
+  }
+
+
+  // Private methods
+  // ---------------
+
+  /**
+   * @param string $value
+   * @return string
+   */
+  private function fixTargetLanguage(string $value): string {
+    return array_key_exists(strtolower($value), $this->_localeReplacements)
+      ? $this->_localeReplacements[strtolower($value)]
+      : $value;
   }
 
 
